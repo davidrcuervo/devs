f48482460a89132301459e035c15deab0867d39d
myself <myself@la-etienda.com>
2016-11-24 13:58:06 -0500

    Create project

---
+++ notes/src/com/laetienda/notes/bin/Command.java
@@ -0,0 +1,10 @@
+package com.laetienda.notes.bin;
+
+public class Command {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello word");
+	}
+
+}
