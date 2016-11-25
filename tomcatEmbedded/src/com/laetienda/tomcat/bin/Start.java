b1f72ddf749a96ba8ec975800a5d1b65f36159e8
myself <myself@la-etienda.com>
2016-11-24 21:57:42 -0500

    Marven creation and first tests

--- tomcatEmbedded/src/com/laetienda/tomcat/bin/Start.java
+++ tomcatEmbedded/src/com/laetienda/tomcat/bin/Start.java
@@ -1,9 +1,13 @@
 package com.laetienda.tomcat.bin;
 
+import java.io.File;
+
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello Word");
+		System.out.println(System.getProperty("user.dir") + File.separator + "..");
 
 	}
 
--- tomcatEmbedded/src/com/laetienda/tomcat/lib/Service.java
+++ tomcatEmbedded/src/com/laetienda/tomcat/lib/Service.java
@@ -1,5 +1,62 @@
 package com.laetienda.tomcat.lib;
 
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.File;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+
 public class Service {
+	
+	Tomcat tomcat = new Tomcat();
+	String directory;
+	Integer port;
+	Context context;
+	
+	Service(){
+		tomcat = new Tomcat();
+		directory = System.getProperty("user.dir") + File.separator + ".." + File.separator + "tomcat";
+		port = 8080;
+	}
+	
+	public void start(){
+		tomcat.setPort(port);
+		tomcat.setBaseDir(directory);
+		tomcat.getHost().setAppBase(directory);
+		tomcat.getHost().setAutoDeploy(true);
+		tomcat.getHost().setDeployOnStartup(true);
+		
+	}
+
+	/**
+	 * @return the directory
+	 */
+	public String getDirectory() {
+		return directory;
+	}
+
+	/**
+	 * @param directory the directory to set
+	 */
+	public void setDirectory(String directory) {
+		this.directory = directory;
+	}
+
+	/**
+	 * @return the port
+	 */
+	public Integer getPort() {
+		return port;
+	}
+
+	/**
+	 * @param port the port to set
+	 */
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+	
+	
 
 }
