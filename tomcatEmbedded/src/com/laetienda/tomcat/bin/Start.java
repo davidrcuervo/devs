5d705e220ee24253579304eda86bc8816cedc68e
myself <myself@la-etienda.com>
2016-11-24 20:11:19 -0500

    Building project

---
+++ tomcatEmbedded/src/com/laetienda/tomcat/bin/Start.java
@@ -0,0 +1,10 @@
+package com.laetienda.tomcat.bin;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ tomcatEmbedded/src/com/laetienda/tomcat/lib/Service.java
@@ -0,0 +1,5 @@
+package com.laetienda.tomcat.lib;
+
+public class Service {
+
+}
