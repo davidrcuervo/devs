3f84f571d31243a823eb44266ea794ffd94be959
myself <myself@la-etienda.com>
2017-05-18 16:11:45 -0400

    TomcatException added

---
+++ notes_0.1.1/src/main/java/com/laetienda/tomcat/TomcatException.java
@@ -0,0 +1,27 @@
+package com.laetienda.tomcat;
+
+import com.laetienda.AppException;
+
+public class TomcatException extends AppException {
+	private static final long serialVersionUID = 1L;
+	
+	public TomcatException() { 
+		super(); 
+	}
+
+	public TomcatException(String message) { 
+		super(message); 
+	}
+	
+	public TomcatException(String message, Throwable cause) { 
+		super(message, cause); 
+	}
+	
+	public TomcatException(Throwable cause) { 
+		super(cause); 
+	}
+	
+	public TomcatException(String message, Exception parent){
+		super(message, parent);
+	}
+}
