c3df7a2d975cf04a5e3d9cd74ac52037612f9a27
mac <mac@la-etienda.com>
2018-02-27 22:30:18 -0500

    Mail files.

---
+++ notes_0.1.1/src/main/java/com/laetienda/mail/Email.java
@@ -0,0 +1,23 @@
+package com.laetienda.mail;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.internet.InternetAddress;
+
+public class Email {
+
+	private InternetAddress from;
+	private String password;
+	private Integer port;
+	private String host;
+	private Properties props;
+	
+	public Email() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
